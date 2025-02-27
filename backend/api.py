3159f154bfd124786a494a9fcd61a4178509e050
Aaron Davila <aaron.davila@csu.fullerton.edu>
2025-02-26 19:52:02 -0800

    Removed API key and added it locally to a .env file. Added from dotenv to import the key. added base url os.getenv(). Added 404 error in HTTPSExceptions Used Uvicorn for Server Hosting Backend

--- backend/api.py
+++ backend/api.py
@@ -1,13 +1,14 @@
-import requests
 import os
-import asyncio
-from fastapi import FastAPI
+import httpx
+from fastapi import FastAPI, HTTPException
+from dotenv import load_dotenv
+
+load_dotenv('api.env')
 
 app = FastAPI()
 
-api_key = os.getenv("COINMARKETCAP_API_KEY", "690e454d-4fd1-4154-810a-554c1b0a9619")
-base_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
-
+api_key = os.getenv("COINMARKETCAP_API_KEY")
+base_url = os.getenv("DATABASE_URL", "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest")
 def get_crypto_price(crypto_symbol="BTC"):
     headers = {
         "X-CMC_PRO_API_KEY": api_key
@@ -16,17 +17,32 @@ def get_crypto_price(crypto_symbol="BTC"):
         "symbol": crypto_symbol,
         "convert": "USD"
     }
-    response = requests.get(base_url, headers=headers, params=params).json()
+    print(f"base_url: {base_url}")
+    response = httpx.get(base_url, headers=headers, params=params).json()
     
     if "data" in response and crypto_symbol in response["data"]:
-        return {
+        price = response["data"][crypto_symbol]["quote"]["USD"]["price"],
+        market_cap = response["data"][crypto_symbol]["quote"]["USD"]["market_cap"],
+        volume_24h = response["data"][crypto_symbol]["quote"]["USD"]["volume_24h"]
+       
+        """ return {
           "price": response["data"][crypto_symbol]["quote"]["USD"]["price"],
           "market_cap": response["data"][crypto_symbol]["quote"]["USD"]["market_cap"],
           "volume_24h": response["data"][crypto_symbol]["quote"]["USD"]["volume_24h"]
-        }
-    return None
+        } """
+    return {
+        "Price": price,
+        "market_cap": market_cap,
+        "volume_24h": volume_24h
+    }
 
+@app.get('/')
+def read_root() :
+    return {"System Message" : "Today's Market"}
 @app.get("/crypto/{symbol}")
 def crypto_price(symbol: str):
     data = get_crypto_price(symbol.upper())
-    return {"crypto": symbol.upper(), **data}
+    if data :
+        return {"crypto": symbol.upper(), **data}
+    else :
+        raise HTTPException(status_code=404, detail="Sorry, this Cryptocurrency is not availble right now")
