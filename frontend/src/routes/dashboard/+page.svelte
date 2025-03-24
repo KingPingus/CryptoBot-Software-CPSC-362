5b5e9b48f5a34af1a41afcb651155e3ed71e15f8
JasonDaRuler <khaujason@hotmail.com>
2025-03-23 17:59:56 -0700

    Added a login screen and log out function

--- frontend/src/routes/dashboard/+page.svelte
+++ frontend/src/routes/dashboard/+page.svelte
@@ -45,28 +45,33 @@
             class="text-muted-foreground hover:text-primary flex items-center gap-3 rounded-lg px-3 py-2 transition-all"
           >
             <ChartLine class="h-4 w-4" />
-            Profile
+            Dashboard
           </a>
           <a
             href="##"
-            class="text-muted-foreground hover:text-primary flex items-center gap-3 rounded-lg px-3 py-2 transition-all"
+            class="bg-muted text-primary hover:text-primary flex items-center gap-3 rounded-lg px-3 py-2 transition-all"
           >
             <Coins class="h-4 w-4" />
-            Settings
+            Coin List
+            <Badge
+              class="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full"
+            >
+              0
+            </Badge>
           </a>
-          <!-- <a
+          <a
             href="##"
             class="text-muted-foreground hover:text-primary flex items-center gap-3 rounded-lg px-3 py-2 transition-all"
           >
             <Package class="h-4 w-4" />
             Server Options
-          </a> -->
+          </a>
           <a
             href="##"
             class="text-muted-foreground hover:text-primary flex items-center gap-3 rounded-lg px-3 py-2 transition-all"
           >
             <Headset class="h-4 w-4" />
-            Logout
+            Contact Us
           </a>
         </nav>
       </div>
@@ -165,7 +170,7 @@
               aria-current={page.url.pathname === '/account' ? 'page' : undefined}>
               <button type="button">Profile</button>
             </a>
-            </DropdownMenu.Label>
+          </DropdownMenu.Label>
           <DropdownMenu.Separator />
           <DropdownMenu.Item><button type="button">Settings</button></DropdownMenu.Item>
           <DropdownMenu.Item><button type="button">Support</button></DropdownMenu.Item>
@@ -176,9 +181,17 @@
     </header>
     <main class="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
       <div class="flex items-center">
-        <h1 class="text-lg font-semibold md:text-2xl">Profile</h1>
+        <h1 class="text-lg font-semibold md:text-2xl">Coin List</h1>
       </div>
-      
+      <div class="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
+        <div class="flex flex-col items-center gap-1 text-center">
+          <h3 class="text-2xl font-bold tracking-tight">You have no coins to track.</h3>
+          <p class="text-muted-foreground text-sm">
+            You can start seeing graphs as soon as you add a coin.
+          </p>
+          <Button class="mt-4">Add Coin</Button>
+        </div>
+      </div>
     </main>
   </div>
 </div>
